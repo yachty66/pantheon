dc721714173d3eb2f7b567755bd8eca0d2932763
Max Hager <68147541+yachty66@users.noreply.github.com>
2024-05-31 18:21:09 -0700

    redploy key

--- replaceApiKey.js
+++ replaceApiKey.js
@@ -6,6 +6,15 @@ const htmlPath = path.join(__dirname, 'webpage', 'index.html');
 let htmlContent = fs.readFileSync(htmlPath, 'utf8');
 
 const apiKey = process.env.GOOGLE_MAPS_API_KEY; // Ensure this is set in Vercel's environment variables
+
+if (!apiKey) {
+    console.error('API key is not set in the environment variables.');
+    process.exit(1);
+}
+
+console.log('API key found:', apiKey);
+
 htmlContent = htmlContent.replace('__API_KEY__', apiKey);
 
 fs.writeFileSync(htmlPath, htmlContent);
+console.log('API key replaced successfully.');
