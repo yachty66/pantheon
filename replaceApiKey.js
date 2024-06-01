761d08c4c4088171511147c738efd63f89a235b1
Max Hager <68147541+yachty66@users.noreply.github.com>
2024-05-31 18:16:21 -0700

    new adjustments

--- replaceApiKey.js
+++ replaceApiKey.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
-const htmlPath = path.join(__dirname, 'webpage', 'index.html'); // Adjust this path if necessary
+// Adjust the path to point to the correct location of index.html
+const htmlPath = path.join(__dirname, 'webpage', 'index.html');
 let htmlContent = fs.readFileSync(htmlPath, 'utf8');
 
 const apiKey = process.env.GOOGLE_MAPS_API_KEY; // Ensure this is set in Vercel's environment variables
