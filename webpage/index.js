2d7db5152b24fce0c268399369ea893b46c38b0f
Max Hager <68147541+yachty66@users.noreply.github.com>
2024-05-31 23:55:46 -0700

    adjusted zoom

--- webpage/index.js
+++ webpage/index.js
@@ -7,7 +7,7 @@ async function initMap() {
 
   map = new google.maps.Map(document.getElementById("map"), {
     center: new google.maps.LatLng(37.77717257607855, -122.40821649247209),
-    zoom: 12,
+    zoom: 14,
     mapId: "DEMO_MAP_ID",
   });
 
