8ce48e274c3e96c090e3b7334eb6107d63adb92e
Max Hager <68147541+yachty66@users.noreply.github.com>
2024-06-26 04:43:56 -0700

    m

--- resident_advisor.py
+++ resident_advisor.py
@@ -18,12 +18,6 @@ def main():
     proxy_username = os.environ.get("PROXY_USERNAME")
     proxy_password = os.environ.get("PROXY_PASSWORD")
     proxy_url = os.environ.get("PROXY_URL")
-    supabase_url=os.environ.get("SUPABASE_URL")
-    print("TEST", os.environ.get("TEST"))
-    print("proxy username", proxy_username)
-    print("supabaseurl", supabase_url)
-    print("proxy password", proxy_password)
-    print("proxy url", proxy_url)
     html_content_start_page = fetch_start_page(
         url, proxy_username, proxy_password, proxy_url
     )
