a0f5cc5e947165e08b5fdc0391993eb40f25a37d
Max Hager <68147541+yachty66@users.noreply.github.com>
2024-06-26 03:37:06 -0700

    testing

--- api.py
+++ api.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from supabase import create_client, Client
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 import os
 
-load_dotenv()
+#load_dotenv()
 
 app = FastAPI()
 
@@ -17,9 +17,9 @@ app = FastAPI()
 #     allow_headers=["*"],
 # )
 
-url: str = os.getenv("SUPABASE_URL")
-key: str = os.getenv("SUPABASE_KEY")
-supabase: Client = create_client(url, key)
+# url: str = os.getenv("SUPABASE_URL")
+# key: str = os.getenv("SUPABASE_KEY")
+# supabase: Client = create_client(url, key)
 
 @app.get("/")
 async def root():
@@ -29,12 +29,12 @@ async def root():
 async def hello():
     return {'res': 'pong'}
 
-@app.get("/api/events")
-async def get_events():
-    response = supabase.table("resident_advisor").select("*").order('created_at', desc=True).limit(1).execute()
-    if hasattr(response, 'error') and response.error:
-        return {"error": str(response.error)}
-    elif hasattr(response, 'data') and response.data:
-        return {"data": response.data[0]}
-    else:
-        return {"error": "No data found or unexpected response structure"}
+# @app.get("/api/events")
+# async def get_events():
+#     response = supabase.table("resident_advisor").select("*").order('created_at', desc=True).limit(1).execute()
+#     if hasattr(response, 'error') and response.error:
+#         return {"error": str(response.error)}
+#     elif hasattr(response, 'data') and response.data:
+#         return {"data": response.data[0]}
+#     else:
+#         return {"error": "No data found or unexpected response structure"}
